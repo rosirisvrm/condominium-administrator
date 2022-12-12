1d8035ffe785ee3299821ab6cd1c556974d0a4e1
Rosiris Romero <rosirisvrm@gmaiil.com>
2022-12-12 13:58:26 -0400

    Create Survey Step 2

--- src/pages/Surveys/CreateSurvey.js
+++ src/pages/Surveys/CreateSurvey.js
@@ -4,10 +4,29 @@ import { useNavigate } from 'react-router-dom';
 import { useForm } from "react-hook-form";
 import PropTypes from 'prop-types';
 // @mui
-import { Container, Typography, Grid, Box, Stepper, Step, StepLabel } from '@mui/material';
+import { 
+  Container, 
+  Typography, 
+  Grid, 
+  Box, 
+  Stepper, 
+  Step, 
+  StepLabel, 
+  Checkbox, 
+  FormControlLabel,
+  IconButton
+} from '@mui/material';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 import { styled } from '@mui/material/styles';
 // components
 import Page from '../../components/Page';
+import Iconify from '../../components/Iconify';
 import { FormCard } from '../../components/FormCard';
 import { Input } from '../../components/Input';
 import { OutlinedButton } from '../../components/OutlinedButton';
@@ -30,14 +49,7 @@ const StepperButtonsContainer = styled(Box)(({ theme }) => ({
   marginTop: theme.spacing(8),
 }))
 
-StepperButtons.propTypes = {
-  handleBack: PropTypes.func,
-  handleNext: PropTypes.func,
-  activeStep: PropTypes.number,
-  smUp: PropTypes.bool,
-}
-
-const StepperButtons = ({ handleBack, handleNext, activeStep, smUp }) => (
+const StepperButtons = ({ handleBack, handleNext, activeStep, steps, smUp }) => (
   <StepperButtonsContainer>
     {activeStep === 0 ? 
       <OutlinedButton 
@@ -60,14 +72,24 @@ const StepperButtons = ({ handleBack, handleNext, activeStep, smUp }) => (
 
     <Box sx={{ flex: '1 1 auto' }} />
 
-    <ContainedButton 
-      onClick={handleNext}
-      defaultPadding
-    >
-      Siguiente
-    </ContainedButton>
+    {activeStep === steps - 1 ? 
+      <ContainedButton type='submit' defaultPadding>
+        Finalizar
+      </ContainedButton>
+      :
+      <ContainedButton onClick={handleNext} defaultPadding>
+        Siguiente
+      </ContainedButton>
+    }
   </StepperButtonsContainer>
 )
+
+StepperButtons.propTypes = {
+  handleBack: PropTypes.func,
+  handleNext: PropTypes.func,
+  activeStep: PropTypes.number,
+  smUp: PropTypes.bool,
+}
 
 // ----------------------------------------------------------------------
 
@@ -80,13 +102,15 @@ function CreateSurvey() {
 
   const navigate = useNavigate()
 
-  const { control, handleSubmit, formState: { errors } } = useForm({
+  const { control, handleSubmit, formState: { errors }, setValue, getValues, watch, setError, clearErrors } = useForm({
     defaultValues: {
       title: '',
       description: '',
-      initilDate: new Date(),
+      initialDate: new Date(),
       finalDate: new Date(),
       file: '',
+      question: '',
+      questionDescription: ''
     }
   });
 
@@ -109,33 +133,33 @@ function CreateSurvey() {
   const [file, setFile] = React.useState(null)
 
   const onSubmit = (event) => {
-    dispatch(setLoadingCreateUser(true))
+    // dispatch(setLoadingCreateUser(true))
 
     console.log('submit');
     console.log('event ', event);
 
-    const body = {
-     ...event,
-     file,
-    }
+    // const body = {
+    //  ...event,
+    //  file,
+    // }
 
-    setTimeout(() => {
-      const createUserRequest = async () => {
-        const res = await postUser(body)
+    // setTimeout(() => {
+    //   const createUserRequest = async () => {
+    //     const res = await postUser(body)
   
-        dispatch(setLoadingCreateUser(false))
-        setColor(res ? 'success' : 'error')
-        setOpen(true);
+    //     dispatch(setLoadingCreateUser(false))
+    //     setColor(res ? 'success' : 'error')
+    //     setOpen(true);
 
-        if(res){
-          setTimeout(() => {
-            navigate('/dashboard/encuestas')
-          }, 2000)
-        }
-      }
+    //     if(res){
+    //       setTimeout(() => {
+    //         navigate('/dashboard/encuestas')
+    //       }, 2000)
+    //     }
+    //   }
 
-      createUserRequest()
-    }, 2000)
+    //   createUserRequest()
+    // }, 2000)
   }
 
   let spacing = 2;
@@ -143,16 +167,41 @@ function CreateSurvey() {
   if(mdUp) spacing = 12;
 
   const [activeStep, setActiveStep] = React.useState(0);
+  const [validationMessage, setValidationMessage] = React.useState('');
 
   const steps = ['Información de la encuesta', 'Añadir preguntas', 'Seleccionar usuarios'];
 
   const handleNext = () => {
 
-    if(activeStep === steps.length - 1){
-      navigate('/dashboard/encuestas')
+    let isValid = true;
+
+    if(activeStep === 0){
+      if(!getValues('title')){
+        setError("title", { type: 'required', message: 'El campo es requerido' });
+        isValid = false
+      }else{
+        clearErrors('title');
+      }
+      if(!getValues('description')){
+        setError("description", { type: 'required', message: 'El campo es requerido' });
+        isValid = false
+      }else{
+        clearErrors('description');
+      }
     }
 
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    if(activeStep === 1){
+      if(questions.length === 0){
+        setValidationMessage('No ha añadido ninguna pregunta, por favor añadir')
+        isValid = false
+      }else{
+        setValidationMessage('')
+      }
+    }
+
+    if(isValid){
+      setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    } 
   };
 
   const handleBack = () => {
@@ -163,6 +212,41 @@ function CreateSurvey() {
     setFileName(files[0]?.name || '')
     setFile(files[0])
   }
+
+  const [checked, setChecked] = React.useState(false);
+
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
+
+  const [questions, setQuestions] = React.useState([])
+
+  const addQuestion = () => {
+
+    const newQuestions = [... questions, {
+      question: getValues('question'),
+      questionDescription: getValues('questionDescription'),
+      type: checked ? { label: 'Cerrada' , value: 1 } : { label: 'Abierta', value: 0 }
+    }]
+
+    setQuestions(newQuestions)
+
+    setValue("question", '')
+    setValue("questionDescription", '')
+    setValidationMessage('')
+  }
+
+  const deleteQuestion = (index) => {
+    console.log('eliminando pregunta')
+
+    const newQuestions = [... questions]
+
+    newQuestions.splice(index, 1)
+
+    setQuestions(newQuestions)
+  }
+
+  const headers = ['Pregunta', 'Tipo', ''];
 
   return (
     <Page title="Crear Encuesta">
@@ -192,12 +276,6 @@ function CreateSurvey() {
                     placeholder='Ingrese un título'
                     type='text'
                     control={control}
-                    validations={{
-                      required: {
-                        value: true,
-                        message: 'El campo es requerido'
-                      },
-                    }}
                     error={errors.title}
                   />
                 </Grid>
@@ -210,12 +288,6 @@ function CreateSurvey() {
                     multiline
                     type='text'
                     control={control}
-                    validations={{
-                      required: {
-                        value: true,
-                        message: 'El campo es requerido'
-                      },
-                    }}
                     error={errors.description}
                   />
                 </Grid>
@@ -228,12 +300,6 @@ function CreateSurvey() {
                       placeholder='Selecciona una fecha'
                       isDate
                       control={control}
-                      validations={{
-                        required: {
-                          value: true,
-                          message: 'El campo es requerido'
-                        },
-                      }}
                       error={errors.initialDate}
                     />
                   </Grid>
@@ -244,12 +310,6 @@ function CreateSurvey() {
                       placeholder='Selecciona una fecha'
                       isDate
                       control={control}
-                      validations={{
-                        required: {
-                          value: true,
-                          message: 'El campo es requerido'
-                        },
-                      }}
                       error={errors.finalDate}
                     />
                   </Grid>
@@ -261,41 +321,121 @@ function CreateSurvey() {
                     label='Asociar Archivo'
                     isFileUpload
                     control={control}
+                    error={errors.file}
+                    callback={handleFileUpload}
+                    helperText={fileName}
+                  />
+                </Grid>
+
+              </Grid>
+            )}
+
+            {activeStep === 1 && (
+              <Grid container spacing={2}>
+
+                <Grid item xs={12}>
+                  <Input
+                    name='question'
+                    label='Pregunta'
+                    placeholder='Ingrese una pregunta'
+                    type='text'
+                    control={control}
                     validations={{
                       required: {
                         value: true,
                         message: 'El campo es requerido'
                       },
                     }}
-                    error={errors.file}
-                    callback={handleFileUpload}
-                    helperText={fileName}
+                    error={errors.question}
                   />
                 </Grid>
+
+                <Grid item xs={12}>
+                  <Input
+                    name='questionDescription'
+                    label='Descripción de la pregunta'
+                    placeholder='Ingrese la descripción de la pregunta'
+                    type='text'
+                    control={control}
+                    validations={{
+                      required: {
+                        value: true,
+                        message: 'El campo es requerido'
+                      },
+                    }}
+                    error={errors.questionDescription}
+                  />
+                </Grid>
+
+                <FormControlLabel sx={{ pt: 2, pl: 2 }} label="Pregunta cerrada" control={
+                  <Checkbox
+                    checked={checked}
+                    onChange={handleChange}
+                    inputProps={{ 'aria-label': 'controlled' }}
+                  />
+                }/>
+
+                <Grid container item direction="row" justifyContent="flex-end" alignItems="flex-end">
+                  <OutlinedButton size='small' onClick={addQuestion} disabled={(!watch("question"))}>
+                    Añadir pregunta
+                  </OutlinedButton>
+                </Grid>
+
+                <Typography variant="h6" sx={{ pt: 2, pl: 2 }}>
+                  Preguntas añadidas
+                </Typography>
+
+                <TableContainer component={Paper} sx={{ pl: 2 }}>
+                  <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                    <TableHead>
+                      <TableRow>
+                        {headers.map((header, index) => (
+                          <TableCell key={index}>{header}</TableCell>  
+                        ))}
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {questions.map((row, index) => (
+                        <TableRow key={index} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                          <TableCell component="th" scope="row">{row.question}</TableCell>
+                          <TableCell>{row.type.label}</TableCell>
+                          <TableCell>
+                            <IconButton onClick={() => deleteQuestion(index)} sx={{ p: 0 }}>
+                              <Iconify icon="eva:trash-2-outline" width={24} height={24} />
+                            </IconButton>
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+
+                {validationMessage && 
+                  <Typography variant="span" sx={{ pt: 2, pl: 2, color: 'red' }}>
+                    {validationMessage}
+                  </Typography>
+                }
                 
-                <StepperButtons handleBack={handleBack} handleNext={handleNext} activeStep={activeStep} smUp={smUp} />
-
               </Grid>
-            )}
-
-            {activeStep === 1 && (
-              <>
-                <Typography sx={{ mt: 2, mb: 1 }}>Step {activeStep + 1}</Typography>
-                <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-                  <StepperButtons handleBack={handleBack} handleNext={handleNext} />
-                </Box>
-              </>
             )}
 
             {activeStep === 2 && (
               <>
                 <Typography sx={{ mt: 2, mb: 1 }}>Step {activeStep + 1}</Typography>
                 <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-                  <StepperButtons handleBack={handleBack} handleNext={handleNext} />
+                  Hola
                 </Box>
               </>
             )}
         
+            <StepperButtons
+              handleBack={handleBack} 
+              handleNext={handleNext} 
+              activeStep={activeStep} 
+              steps={steps.length} 
+              smUp={smUp} 
+            />
+            
           </form>
         </FormCard>
 
